2f6329418fa30c6993a36448b23c0900fc818d7d
Sofia Anjos <emmanuelesofia@gmail.com>
2022-07-19 23:04:36 -0300

    global context teste

--- pokedex/src/Pages/Home/Home.js
+++ pokedex/src/Pages/Home/Home.js
@@ -1,12 +1,7 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { GlobalContextState } from "../../Context/globalContextState";
 
-
-export const Home = () =>{
-
-return (
-    <div>
-        <h1>Home</h1>
-    </div>
-)
-
-}
+export const Home = () => {
+  const textinho = useContext(GlobalContextState);
+  return <div>{textinho}</div>;
+};
