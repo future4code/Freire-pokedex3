19ad35c39a5f1246dc17cc2f88c47212c27dfbd4
Sofia Anjos <emmanuelesofia@gmail.com>
2022-07-22 18:00:26 -0300

    problemas no terminal

--- pokedex/src/Pages/Pokedex/Pokedex.js
+++ pokedex/src/Pages/Pokedex/Pokedex.js
@@ -1,13 +1,10 @@
-import React from 'react';
-import { Header } from '../../Components/Header/Header';
+import React from "react";
+import { Header } from "../../Components/Header/Header";
 
-export const Pokedex = ()=>{
-
-    
-    return (
+export const Pokedex = () => {
+  return (
     <div>
-        <Header page={'pokedex'}/>
+      <Header page={"pokedex"} />
     </div>
-    )
-
-}
+  );
+};
